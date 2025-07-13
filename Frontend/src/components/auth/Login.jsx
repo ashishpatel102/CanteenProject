a33a72d8bcc51616f4645c203ba3e8ac8407dc99
Ashish Patel <ashishpatel84h@gmail.com>
2025-07-13 11:39:54 +0530

    Update Login.jsx

--- Frontend/src/components/auth/Login.jsx
+++ Frontend/src/components/auth/Login.jsx
@@ -78,7 +78,7 @@ const Login = ({ setError, error }) => {
               placeholder="Enter username"
               onChange={FormHandler}
               name="Username"
-              value={(getFormValue.Username)}
+              value={(getFormValue.Username).trim().replace('', '_')}
               required
             />
           </div>
